7595f748e2a9e16b3c05f19ed2237339be89a746
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2023-04-13 14:20:53 +0800

    Beans

--- src/main/java/net/javaguides/springboot/config/RabbitMQConfig.java
+++ src/main/java/net/javaguides/springboot/config/RabbitMQConfig.java
@@ -1,7 +1,11 @@
 package net.javaguides.springboot.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,8 +14,30 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue.name}")
     private String queue;
 
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
     // spring bean for rabbitmq queue
+    @Bean
     public Queue queue(){
         return new Queue(queue);
+    }
+
+    // Spring bean for rabbitmq exchange
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+
+    // Binding between queue and exchange using routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
     }
 }
