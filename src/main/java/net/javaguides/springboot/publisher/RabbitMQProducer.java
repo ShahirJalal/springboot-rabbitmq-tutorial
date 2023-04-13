23e642a49a66789593e4e92ee399c87bd3a7e747
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2023-04-13 15:40:19 +0800

    update

--- src/main/java/net/javaguides/springboot/publisher/RabbitMQProducer.java
+++ src/main/java/net/javaguides/springboot/publisher/RabbitMQProducer.java
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMQProducer {
 
-    @Value("$rabbitmq.exchange.name")
+    @Value("${rabbitmq.exchange.name}")
     private String exchange;
 
-    @Value("$rabbitmq.routing.key")
+    @Value("${rabbitmq.routing.key}")
     private String routingKey;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
